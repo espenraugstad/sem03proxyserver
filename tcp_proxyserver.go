88d7be2e842fd6c6ce33e9ecb2f22368cf1ad3ad
espenraugstad <espen.raugstad@gmail.com>
2023-04-25 00:24:27 +0000

    Sniffed + Added port to proxy server.

--- tcp_proxyserver.go
+++ tcp_proxyserver.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	proxyServer, err := net.Listen("tcp", "172.17.0.2:")
+	proxyServer, err := net.Listen("tcp", "172.17.0.2:8008")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,7 +27,7 @@ func main() {
 				defer client.Close()
 				log.Println("Connected to proxy")
 
-				server, err := net.Dial("tcp", "172.17.0.3:38739")
+				server, err := net.Dial("tcp", "172.17.0.3:8000")
                 if err != nil {
 					log.Println(err)
 					return
